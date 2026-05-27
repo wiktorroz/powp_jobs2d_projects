3e20be73e556c0f25e48a3f4d17aa383e72fd2f6
Wiktor Różański <wik2003@outlook.com>
2026-05-27 12:45:15 +0200

    I changed location of files to optional_drivers folder.

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/optional_drivers/RecordingDriver.java
@@ -0,0 +1,84 @@
+package edu.kis.powp.jobs2d.drivers.optional_drivers;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import edu.kis.powp.jobs2d.command.DriverCommand;
+import edu.kis.powp.jobs2d.command.OperateToCommand;
+import edu.kis.powp.jobs2d.command.SetPositionCommand;
+import edu.kis.powp.jobs2d.drivers.DecoratorDriver;
+import edu.kis.powp.jobs2d.drivers.visitor.DriverVisitor;
+import edu.kis.powp.jobs2d.drivers.visitor.VisitableDriver;
+
+//DecoratorDriver extends VisitableDriver and adds getTarget/setTarget contract
+
+/**
+ * Decorator driver that records all calls as command objects.
+ * Recording can be temporarily disabled (used during playback).
+ */
+public class RecordingDriver implements DecoratorDriver {
+
+    private VisitableDriver target;
+    private final List<DriverCommand> recorded = new ArrayList<>();
+    private boolean recordingEnabled = true;
+
+    public RecordingDriver(VisitableDriver initialTarget) {
+        this.target = initialTarget;
+    }
+
+    public synchronized void setTarget(VisitableDriver target) {
+        this.target = target;
+    }
+
+    public synchronized VisitableDriver getTarget() {
+        return target;
+    }
+
+    /**
+     * Enable or disable recording of subsequent driver calls.
+     * When disabled, setPosition/operateTo will still delegate to the target
+     * but won't add commands to recorded list.
+     */
+    public synchronized void setRecordingEnabled(boolean enabled) {
+        this.recordingEnabled = enabled;
+    }
+
+    public synchronized boolean isRecordingEnabled() {
+        return recordingEnabled;
+    }
+
+    public synchronized void clearRecorded() {
+        recorded.clear();
+    }
+
+    public synchronized List<DriverCommand> getRecordedCommands() {
+        return Collections.unmodifiableList(new ArrayList<>(recorded));
+    }
+
+    @Override
+    public synchronized void setPosition(int x, int y) {
+        if (recordingEnabled) {
+            recorded.add(new SetPositionCommand(x, y));
+        }
+        target.setPosition(x, y);
+    }
+
+    @Override
+    public synchronized void operateTo(int x, int y) {
+        if (recordingEnabled) {
+            recorded.add(new OperateToCommand(x, y));
+        }
+        target.operateTo(x, y);
+    }
+
+    @Override
+    public synchronized String toString() {
+        return "RecordingDriver -> " + target;
+    }
+
+    @Override
+    public void accept(DriverVisitor visitor) {
+        visitor.visit(this);
+    }
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/optional_drivers/TrackingLoggerDriver.java
@@ -0,0 +1,34 @@
+package edu.kis.powp.jobs2d.drivers.optional_drivers;
+
+import edu.kis.powp.jobs2d.drivers.visitor.DriverVisitor;
+import edu.kis.powp.jobs2d.drivers.visitor.VisitableDriver;
+
+import java.util.logging.Logger;
+
+public class TrackingLoggerDriver implements VisitableDriver {
+    // Używamy tego samego globalnego loggera, żeby zachować kompatybilność z konfiguracją aplikacji
+    private final Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+    private int operationCount = 0;
+
+    @Override
+    public void setPosition(int x, int y) {
+        this.logger.info("Position set to x: " + x + ", y: " + y + " [" + operationCount + "]");
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        operationCount++;
+        this.logger.info("Operate to x: " + x + ", y: " + y + " [" + operationCount + "]");
+    }
+
+    @Override
+    public String toString() {
+        return "Tracking Logger Driver";
+    }
+
+    @Override
+    public void accept(DriverVisitor visitor) {
+        visitor.visit(this);
+    }
+}
