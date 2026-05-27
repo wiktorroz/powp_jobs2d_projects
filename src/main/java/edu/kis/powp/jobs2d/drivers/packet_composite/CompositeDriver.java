2a5437b4a264b0b380aaab144a52c5a9ab8fe3b0
Wiktor Różański <wik2003@outlook.com>
2026-05-27 13:02:39 +0200

    Repaired broken dev-io1 local branch and moved it to new "optional_drivers" branch. In CompositeDriver.java after review I've changed  in addDriver function from drivers to "driver".

--- src/main/java/edu/kis/powp/jobs2d/drivers/packet_composite/CompositeDriver.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/packet_composite/CompositeDriver.java
@@ -22,10 +22,10 @@ public class CompositeDriver implements VisitableDriver {
     }
 
     public void addDriver(VisitableDriver driver) {
-        if(driver == null) {
+        if(drivers == null) {
             throw new IllegalArgumentException("Drivers cannot be null");
         }
-        else if(driver == this){
+        else if(drivers == this){
             throw new IllegalArgumentException("Cannot add itself as a driver");
         }
         drivers.add(driver);
